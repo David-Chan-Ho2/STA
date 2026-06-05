d9c6b9e23fd541e8621ac5dbde0b11f4a873f72a
David-Chan-Ho2 <dho12@charlotte.edu>
2026-06-05 19:10:25 -0400

    remove split from allowed origin

--- server/config/config.py
+++ server/config/config.py
@@ -8,11 +8,11 @@ class Settings:
 
     SECRET_KEY = os.getenv("SECRET_KEY", "6c55fd158b8337b7ac8cfb5f786566a0fcce17e8cb65bbfadefbfbd624565a00")
 
-    DATABASE_URL = os.getenv("DATABASE_URL")
+    DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:password@localhost:5432/sta")
     ALGORITHM = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-    ALLOWED_ORIGINS = os.getenv("ALLOWED_ORIGINS", "http://localhost:3000").split(",")
+    ALLOWED_ORIGINS = os.getenv("ALLOWED_ORIGINS", "http://localhost:3000")
 
     REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379")
     SESSION_TIMEOUT_SECONDS = 1800
