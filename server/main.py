6bdd969cfbbfe69c79abb19b6e068e14ce1d62cc
David-Chan-Ho2 <dho12@charlotte.edu>
2026-06-05 20:15:08 -0400

    clear db for development

--- server/main.py
+++ server/main.py
@@ -1,14 +1,30 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
+from sqlalchemy import text
 
-from config.database import Base, engine
+from config.database import engine
+from models.base import Base
 from config.config import settings
 from api import routers
 
 import models
 
+Base.metadata.drop_all(bind=engine)
 Base.metadata.create_all(bind=engine)
+
+# with engine.begin() as conn:
+#     conn.execute(text("""
+#         CREATE EXTENSION IF NOT EXISTS timescaledb;
+#     """))
+
+#     conn.execute(text("""
+#         SELECT create_hypertable(
+#             'sensor_readings',
+#             'time',
+#             if_not_exists => TRUE
+#         );
+#     """))
 
 app = FastAPI()
 
